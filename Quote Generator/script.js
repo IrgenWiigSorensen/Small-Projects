8d82cc905be7861e1aade5777a9d307bfbc4bf33
Irgen Wiig Sørensen <67002417+IrgenWiigSorensen@users.noreply.github.com>
2023-01-13 13:52:01 +0100

    Updater the generator with twitter button and loading function

--- Quote Generator/script.js
+++ Quote Generator/script.js
@@ -1,14 +1,51 @@
+const quoteContainer = document.getElementById('quote-container');
+const quoteText = document.getElementById('quote');
+const authorText = document.getElementById('author');
+const twitterBtn = document.getElementById('twitter');
+const newQuoteBtn = document.getElementById('new-quote');
+const loader = document.getElementById('loader');
+
+
 
 let apiQuotes = [];
 
+//Show loading 
+function loading(){
+    loader.hidden = false;
+    quoteContainer.hidden = true;
+}
+
+//Hide Loading 
+function complete(){
+    quoteContainer.hidden = false;
+    loader.hidden = true;
+}
+
 // Show New Quote
 function newQuote() {
+    loading();
     //Pick a random quote from ApiQuotes array
-    const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)]; 
+    const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
+    //Check if Author field is blank and replace it with unknown. 
+    if (!quote.author) {
+        authorText.textContent = 'Unknown'
+    } else {
+        authorText.textContent = quote.author;
+    }
+    // Check quote length to determine styling
+    if (quote.text.length > 120) {
+        quoteText.classList.add('long-quote');
+    } else {
+        quoteText.classList.remove('long-quote');
+    }
+    //Set Quote, hide Loader
+    quoteText.textContent = quote.text;
+    complete();
 }
 
 //Get Quotes from API
 async function getQuotes() {
+    loading();
     const apiUrl = 'https://type.fit/api/quotes';
     try {
         const response = await fetch(apiUrl);
@@ -19,14 +56,27 @@ async function getQuotes() {
     }
 }
 
-//On Load
-getQuotes();
+
 
 
 //Same but using array that is saved locally
 /* function newQuote() {
+
     //Pick a random quote from localQuotes which is a local API. 
     const quote = localQuotes[Math.floor(Math.random() * apiQuotes.length)]; 
 }
 
 newQuote(); */
+
+// Tweet a quote 
+function tweetQuote () {
+    const twitterUrl = `https://twitter.com/intent/tweet?text=${quoteText.textContent} - ${authorText.textContent}`;
+    window.open(twitterUrl, '_blank');
+}
+
+//Event listeners: 
+newQuoteBtn.addEventListener('click', newQuote);
+twitterBtn.addEventListener('click', tweetQuote);
+
+//On Load
+getQuotes();
